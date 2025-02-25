7af22bc55387d2934c48c17cdd09c7a3d5873394
Luu Quang Tinh <luuquangtinh6597@gmail.com>
2025-02-25 18:29:39 +0700

    remove breadcrumb

--- app/layout/dashboard.tsx
+++ app/layout/dashboard.tsx
@@ -63,10 +63,6 @@ export default function Dashboard() {
             </Sider>
             <Layout>
                 <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>User</Breadcrumb.Item>
-                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                    </Breadcrumb>
                     <div
                         style={{
                             padding: 24,
